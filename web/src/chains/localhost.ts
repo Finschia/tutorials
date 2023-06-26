d79d46815179ff7556414e197dfb65637fb3e76a
jinseong.cho <jinseong.cho@linecorp.com>
2023-06-27 01:10:35 +0900

    fix:add http

--- web/src/chains/localhost.ts
+++ web/src/chains/localhost.ts
@@ -11,7 +11,7 @@ const linkPriceStep = { low: 0.015, average: 0.015, high: 0.015 };
 export const chainInfo = {
   chainId: "simd-testing",
   chainName: "simd-testing",
-  rpc: "localhost:26657",
+  rpc: "http://localhost:26657",
   rest: "http://localhost:1317",
   bip44: linkBip44,
   bech32Config: {
