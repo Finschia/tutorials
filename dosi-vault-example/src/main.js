3f309bbf74e1221817c22a4a3fd6450776b54d96
jinseong.cho <jinseong.cho@linecorp.com>
2023-06-27 11:36:41 +0900

    export chain info

--- dosi-vault-example/src/main.js
+++ dosi-vault-example/src/main.js
@@ -3,35 +3,7 @@ import { SigningFinschiaClient, FinschiaClient } from "@finschia/finschia";
 import { TxRaw } from "@finschia/finschia-proto/cosmos/tx/v1beta1/tx";
 import "../dosivault-logo.png";
 
-const linkCurrency = {
-  coinDenom: "FNSA",
-  coinMinimalDenom: "cony",
-  coinDecimals: 6,
-  coinGeckoId: "link",
-};
-
-const linkBip44 = { coinType: 438 };
-const linkPriceStep = { low: 0.015, average: 0.015, high: 0.015 };
-
-const chainInfo = {
-  chainId: "simd-testing",
-  chainName: "simd-testing",
-  rpc: "http://localhost:26658",
-  rest: "http://localhost:1317",
-  bip44: linkBip44,
-  bech32Config: {
-    bech32PrefixAccAddr: "link",
-    bech32PrefixAccPub: "linkpub",
-    bech32PrefixValAddr: "linkvaloper",
-    bech32PrefixValPub: "linkvaloperpub",
-    bech32PrefixConsAddr: "linkvalcons",
-    bech32PrefixConsPub: "linkvalconspub",
-  },
-  currencies: [linkCurrency],
-  feeCurrencies: [{ ...linkCurrency, gasPriceStep: linkPriceStep }],
-  stakeCurrency: linkCurrency,
-  features: ["cosmwasm"],
-};
+import { chainInfo } from "./local-chain-info";
 
 window.onload = async () => {
   if (!window.dosiVault) {
