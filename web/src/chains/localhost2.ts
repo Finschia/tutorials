8fc3ccfe5b07cfcd4dff5c89f8e9263b27e9ecdd
jinseong.cho <jinseong.cho@linecorp.com>
2023-06-27 15:28:13 +0900

    change port

--- web/src/chains/localhost2.ts
+++ web/src/chains/localhost2.ts
@@ -11,7 +11,7 @@ const linkPriceStep = { low: 0.015, average: 0.015, high: 0.015 };
 export const chainInfo = {
   chainId: "simd-testing",
   chainName: "simd-testing",
-  rpc: "http://localhost:26659",
+  rpc: "http://localhost:26557",
   rest: "http://localhost:1318",
   bip44: linkBip44,
   bech32Config: {
