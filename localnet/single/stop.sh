c6b80e97bd324eacbb47221d3da7ea5a5bd6c4fd
Jinseong.Cho <ya6675@naver.com>
2023-07-03 10:37:53 +0900

    add localnet (#5)
    * add localnet
    
    * change port
    
    * change readme not to use ./script directory
    
    * add requirement in readme
    
    * rename folder (separate IBC from single)
    
    ---------
    
    Co-authored-by: Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>

---
+++ localnet/single/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+source "$SCRIPT_DIR"/env
+
+if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )" == "true" ]; then
+  echo "Killing finschia container..."
+  docker container kill "$CONTAINER_NAME"
+fi
