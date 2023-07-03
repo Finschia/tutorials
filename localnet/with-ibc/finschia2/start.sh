4ce461cdf24f04d7b97e4bcd541aaf1d7af5fad2
jinseong.cho <jinseong.cho@linecorp.com>
2023-07-04 02:52:21 +0900

    change host ports for finschia2

--- localnet/with-ibc/finschia2/start.sh
+++ localnet/with-ibc/finschia2/start.sh
@@ -4,11 +4,11 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 
 # Please keep this in sync with the Ports overview in HACKING.md
 TENDERMINT_PORT_GUEST="26657"
-TENDERMINT_PORT_HOST="26657"
+TENDERMINT_PORT_HOST="26557"
 API_PORT_GUEST="1317"
-API_PORT_HOST="1317"
+API_PORT_HOST="1318"
 GRPC_PORT_GUEST="9090"
-GRPC_PORT_HOST="9090"
+GRPC_PORT_HOST="9091"
 
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 # shellcheck source=env
@@ -17,7 +17,7 @@ source "${SCRIPT_DIR}"/env
 TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/fnsa.XXXXXXXXX")
 chmod 777 "$TMP_DIR"
 echo "Using temporary dir $TMP_DIR"
-FINSCHIA_LOGFILE="$TMP_DIR/finschia.log"
+FINSCHIA_LOGFILE="$TMP_DIR/finschia2.log"
 
 # Use a fresh volume for every start
 docker volume rm -f fnsad_data
