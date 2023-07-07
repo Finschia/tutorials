d41dfd0f4548c075bab9f32d306fe80f8c19aa30
jinseong.cho <jinseong.cho@linecorp.com>
2023-07-07 20:20:41 +0900

    apply sheelcheck3

--- localnet/with-ibc/finschia2/template/setup.sh
+++ localnet/with-ibc/finschia2/template/setup.sh
@@ -53,7 +53,7 @@ N=9
 # generate normal account keys
 for ((i = 0; i < N; i++))
 do
-  ${FNSAD} keys add account${i} --home="${CHAIN_DIR}" --keyring-backend=test --recover --index=${i} <<< "${TEST_MNEMONIC}"
+  ${FNSAD} keys add account"${i}" --home="${CHAIN_DIR}" --keyring-backend=test --recover --index="${i}" <<< "${TEST_MNEMONIC}"
 done
 # generate multisig key
 ${FNSAD} keys add multisig0 --home="${CHAIN_DIR}" --keyring-backend=test --multisig account0,account1,account2,account3,account4 --multisig-threshold 2
@@ -64,7 +64,7 @@ ${FNSAD} keys add validator0 --home="${CHAIN_DIR}" --keyring-backend=test --reco
 # Add both accounts, with coins to the genesis file
 for ((i = 0; i < N; i++))
 do
-  ${FNSAD} add-genesis-account "$(${FNSAD} keys show account${i} -a --home="${CHAIN_DIR}" --keyring-backend=test)" 100000000000brown,20000000000stake --home="${CHAIN_DIR}"
+  ${FNSAD} add-genesis-account "$(${FNSAD} keys show account"${i}" -a --home="${CHAIN_DIR}" --keyring-backend=test)" 100000000000brown,20000000000stake --home="${CHAIN_DIR}"
 done
 ${FNSAD} add-genesis-account "$(${FNSAD} keys show multisig0 -a --home="${CHAIN_DIR}" --keyring-backend=test)" 100000000000brown,20000000000stake --home="${CHAIN_DIR}"
 ${FNSAD} add-genesis-account "$(${FNSAD} keys show validator0 -a --home="${CHAIN_DIR}" --keyring-backend=test)" 100000000000brown,20000000000stake --home="${CHAIN_DIR}"
