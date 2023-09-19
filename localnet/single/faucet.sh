6634bdc60c9f3d9c930d3040748d5f7be8c27cfe
zemyblue <zemyblue@gmail.com>
2023-09-19 10:29:55 +0900

    fix: difference mnemonic with single node setting in faucet.sh

--- localnet/single/faucet.sh
+++ localnet/single/faucet.sh
@@ -4,23 +4,21 @@ FAUCET_TOKENS=cony \
 FAUCET_CREDIT_AMOUNT_CONY=10000000 \
 FAUCET_REFILL_FACTOR=10 \
 FAUCET_REFILL_THRESHOLD=1 \
-FAUCET_GAS_PRICE=0.1utoy \
+FAUCET_GAS_PRICE=0.01cony \
 FAUCET_CONCURRENCY=3 \
 FAUCET_PATH_PATTERN="m/44'/438'/2'/0/a" \
-FAUCET_MNEMONIC="answer thing blossom wife popular afraid argue rack yellow ride safe chunk music gate abuse idle render refuse assume uncover human say energy include" \
+FAUCET_MNEMONIC="mind flame tobacco sense move hammer drift crime ring globe art gaze cinnamon helmet cruise special produce notable negative wait path scrap recall have" \
 docker run --read-only --rm -d \
 -e FAUCET_ADDRESS_PREFIX \
 -e FAUCET_TOKENS \
--e FAUCET_CONCURRENCY \
 -e FAUCET_CREDIT_AMOUNT_CONY \
 -e FAUCET_REFILL_FACTOR \
 -e FAUCET_REFILL_THRESHOLD \
 -e FAUCET_GAS_PRICE \
--e FAUCET_MNEMONIC \
+-e FAUCET_CONCURRENCY \
 -e FAUCET_PATH_PATTERN \
+-e FAUCET_MNEMONIC \
 -p 8000:8000 \
 --platform=linux/amd64 \
 confio/faucet:0.29.0 \
 start "$DOCKER_HOST_IP:26657"
-#start "192.168.0.53:26657"
-#start "http://$DOCKER_HOST_IP:1317"
