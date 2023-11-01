4b94e2a554cb893dd6f73667006ccdc4f5d3a4d3
zemyblue <zemyblue@gmail.com>
2023-11-01 21:40:58 +0900

    chore: modify `fetchLatestHeight` function to be called every 2 sec.

--- dosi-vault/dosi-vault-example/src/main.js
+++ dosi-vault/dosi-vault-example/src/main.js
@@ -23,17 +23,17 @@ window.onload = async () => {
   await window.dosiVault.enable(chainInfo.chainId);
 };
 
+setInterval(fetchLatestHeight, 2000);
+
 function fetchLatestHeight() {
-  (async () => {
-    fetch(`${chainInfo.rest}/cosmos/base/tendermint/v1beta1/blocks/latest`)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log("height: ", res.block.header.height);
+  fetch(`${chainInfo.rest}/cosmos/base/tendermint/v1beta1/blocks/latest`)
+  .then(res => res.json())
+  .then(res => {
+      console.log("height: ", res.block.header.height);
     })
-      .catch((error) => {
-        console.error("Error: ", error);
-    });
-  })();
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
 }
 
 function getInputData() {
